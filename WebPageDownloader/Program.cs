152a711a2f3c65f8c97e044bc49b15e74f2f6d9f
VictorGrabowski <v.grabowski@betclicgroup.com>
2025-03-12 10:23:21 +0100

    Download specified webpage Tutorial found on Build-your-own-x repo https://github.com/codecrafters-io/build-your-own-x shit is awesome

---
+++ WebPageDownloader/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpClientExample
+{
+    class Downloader
+    {
+        public static string urlToDownload = "https://16bpp.net/";
+        public static string fileName = "16bpp.html";
+
+        public static async Task DownloadWebPage()
+        {
+            Console.WriteLine("Starting download..");
+            using HttpClient httpClient = new HttpClient();
+            HttpResponseMessage resp = await httpClient.GetAsync(urlToDownload);
+
+            if (resp.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Download successful!");
+                byte[] data = await resp.Content.ReadAsByteArrayAsync();
+                FileStream myFileStream = File.Create(fileName);
+                myFileStream.Write(data, 0, data.Length);
+                myFileStream.Close();
+                Console.WriteLine("Saved in file: " + fileName);
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Task dlTask = DownloadWebPage();
+            Console.WriteLine("Wait for the web page to download..");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            dlTask.GetAwaiter().GetResult();
+        }
+    }
+    
+}
